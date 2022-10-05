f9a813d5c7f0edb2c26bd3fc6bddda700487b3a5
A22AngelGP <a22angelgp@iessanclemente.net>
2022-10-05 09:57:49 +0200

    Cambios ojito

--- src/BoletinesJava.java
+++ src/BoletinesJava.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
 public class BoletinesJava {
+    public static void caso1(){
+        System.out.println("Dame un Double: ");
+        Scanner numerito= new Scanner(System.in);
+        double numeritote= Double.parseDouble(String.valueOf(numerito.nextInt()));
+        double d=numeritote;
+        int i=(int)d;
+        System.out.println(i);
+    }
+    public static void caso2(){
+        System.out.println("Dame un Double: ");
+        Scanner numerito= new Scanner(System.in);
+        double numeritote= Double.parseDouble(String.valueOf(numerito.nextInt()));
+        double d=numeritote;
+        int i=(int)d;
+        System.out.println(i);
+    }
+    public static void caso3(){
 
-    public void cambiodetipos(){
+    }
+    public static void caso4(){
 
+    }
+
+    public static void cambiodetipos(){
+        System.out.println("""
+                1. Cambiar de double a int
+                2. Cambiar de Int a double
+                3. Cambiar de int a short\040
+                4. Cambiar de short a int\s""");
+        System.out.println("Digame que quiere hacer caballero: ");
+        Scanner cositas= new Scanner(System.in);
+        String cosotas = String.valueOf(cositas.nextInt());
+        switch (cosotas){
+            case "1"->{caso1();}
+            case "2"->{caso2();}
+            case "3"->{caso3();}
+            case "4"->{caso4();}
+
+        }
     }
 
 
@@ -14,6 +50,7 @@ public class BoletinesJava {
         switch (numeritote) {
             case "1" -> System.out.println("Hola");
             case "2" -> System.out.println("UWU");
+            case "3" -> cambiodetipos();
             default -> throw new IllegalStateException("Unexpected value: " + numerito);
         }
     }
