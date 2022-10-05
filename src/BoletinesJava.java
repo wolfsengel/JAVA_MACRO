e524cb910be60d16bdd6ebc7179141f2db95ce58
A22AngelGP <a22angelgp@iessanclemente.net>
2022-10-05 09:33:20 +0200

    SWITCH READY

--- src/BoletinesJava.java
+++ src/BoletinesJava.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class BoletinesJava {
+
+    public void cambiodetipos(){
+
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Bienvenido");
+        System.out.println("Digame que boletin desea: ");
+        Scanner numerito= new Scanner(System.in);
+        String numeritote= String.valueOf(numerito.nextInt());
+        switch (numeritote) {
+            case "1" -> System.out.println("Hola");
+            case "2" -> System.out.println("UWU");
+            default -> throw new IllegalStateException("Unexpected value: " + numerito);
+        }
     }
 }
