6a9bfcb91e31560440b0e1521cf3a48b3705992c
A22AngelGP <a22angelgp@iessanclemente.net>
2022-10-11 13:11:44 +0200

    ole

--- src/owo.java
+++ src/owo.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class owo {
 
     public static void main(String[]args){
         List<caso> lista=new ArrayList<>();
-        lista.add(new caso("pepe", 45983421));
+        System.out.println("Dime tu nombre: ");
+        Scanner ole = new Scanner(System.in);
+        String olee= ole.nextLine();
+        System.out.println("Dime tu Dni: (Sin letra) ");
+        Scanner oleee = new Scanner(System.in);
+        int oleeee= Integer.parseInt(oleee.nextLine());
+        lista.add(new caso(olee, oleeee));
         lista.add(new caso("pepe", 45983421));
         lista.add(new caso("pepe", 45983421));
         lista.add(new caso("pepe", 45983421));
