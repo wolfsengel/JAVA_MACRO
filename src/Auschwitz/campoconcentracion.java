4798b4d4a513317bbf4a2a421c4e74f8075f0677
Angel Gomez <a22angelgp@iessanclemente.net>
2022-10-19 10:30:08 +0200

    fin :))))))))))

--- src/Auschwitz/campoconcentracion.java
+++ src/Auschwitz/campoconcentracion.java
@@ -1,10 +1,7 @@
 package Auschwitz;
 
-import Auschwitz.declaraciones;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class campoconcentracion {
