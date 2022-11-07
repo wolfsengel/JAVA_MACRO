e6432d10716b60245e4baed788a1d125f1d28bfd
Angel Gomez <a22angelgp@iessanclemente.net>
2022-11-07 09:02:01 +0100

    clase cosas

--- src/tareaproclase/punto2d.java
+++ src/tareaproclase/punto2d.java
@@ -1,19 +1,21 @@
 package tareaproclase;
 
-public class punto2d {
-    private double cx;
-    private double cy;
-    punto2d(){
+import static java.lang.Math.sqrt;
+
+public class Punto2D {
+double cx;
+double cy;
+    Punto2D(){
         this.cx=0.0;
         this.cy=0.0;
     }
-    punto2d(double cx, double cy){
-        this.cx=cx;
-        this.cy=cy;
+    Punto2D(Punto2D pene){
+        this.cx=pene.cx;
+        this.cy= pene.cy;
     }
-    punto2d(punto2d punto){
-        this.cx=punto.cx;
-        this.cy=punto.cy;
+    Punto2D(double x, double y){
+        this.cx=x;
+        this.cy=y;
     }
 
     public double getX() {
@@ -31,23 +33,62 @@ public class punto2d {
     public void setY(double cy) {
         this.cy = cy;
     }
-    void flip(){
+    public void flip(){
         double aux=this.cx;
         this.cx=this.cy;
         this.cy=aux;
     }
-    double dist(punto2d owo){
-        return ((owo.cx-this.cx)*(owo.cx-this.cx))+((owo.cy-this.cy)*(owo.cy-this.cy));
+    public double dist(Punto2D cum){
+        return (sqrt(((this.cx-cum.cx)*(this.cx-cum.cx))+((this.cy- cum.cy)*(this.cy- cum.cy))));
     }
-    double manhattanDist(Punto2D p){
-
+    public double manhattanDist(Punto2D cum){
+        return Math.abs(this.cx-cum.cx)+Math.abs(this.cy- cum.cy);
+    }
+    public double slope(Punto2D cum){
+        return (this.cy-cum.cy)/(this.cx- cum.cx);
     }
     @Override
-    public String toString() {
-        return super.toString();
+    public String toString(){
+        return "Punto2D["+this.cx+","+this.cy+"]";
     }
-//------------------------------------------------------------------------------------------------------------------------------------
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Punto2D p = new Punto2D();
+        System.out.println(p);
+        //__________________________
+        p = new Punto2D(3.2, -4.8);
+        System.out.println(p);
+        //__________________________
+        Punto2D p1 = new Punto2D(4.5, 5.5);
+        Punto2D p2 = new Punto2D(p1);
+        System.out.println(p2);
+        //__________________________
+        p = new Punto2D();
+        p.setX(5.5);
+        p.setY(-2.0);
+        System.out.println(p);
+        //__________________________
+        p = new Punto2D(5.5, -2.0);
+        System.out.println(p.getX() + " " + p.getY());
+        //__________________________
+        p = new Punto2D(3.2, -4.8);
+        p.flip();
+        System.out.println(p);
+        //__________________________
+        p = new Punto2D(-3.0, 4.0);
+        System.out.println(p.dist(new Punto2D()));
+        System.out.println(p.dist(new Punto2D(2.5, 1.5)));
+        //__________________________
+        p1 = new Punto2D(-2.0, 3.0);
+        p2 = new Punto2D(2.0, 1.0);
+        System.out.println(p1.manhattanDist(p2));
+        System.out.println(p2.manhattanDist(p1));
+        //__________________________
+        p1 = new Punto2D(-2.0, 3.0);
+        p2 = new Punto2D(2.0, 1.0);
+        System.out.println(p1.slope(p2));
+        p1.setY(-1);
+        System.out.println(p1.slope(p2));
     }
+
 }
