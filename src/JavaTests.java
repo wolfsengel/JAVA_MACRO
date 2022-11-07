231a4593c118f7948e13862d02cbd70421d10671
Angel Gomez <a22angelgp@iessanclemente.net>
2022-11-07 12:07:33 +0100

    clase cosas

---
+++ src/JavaTests.java
@@ -0,0 +1,24 @@
+public class JavaTests {
+    int x=34;
+    String uwu="pene";
+    String owo="pene";
+    @Override
+    public String toString() {
+        return " "+x;
+    }
+    String cum(){
+        if(uwu.compareTo(owo)<0){
+            return "CUM";
+        } else if (uwu.compareTo(owo)>0) {
+            return "MUC";
+        }else return "SEMINAL";
+    }
+static class uwu{
+        public static void main(String[] args) {
+            JavaTests MC= new JavaTests();
+            System.out.println(MC.cum());
+            System.out.println(MC.uwu.equals(MC.owo));
+        }
+    }
+
+}
