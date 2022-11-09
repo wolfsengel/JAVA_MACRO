92e5bec2c1859a7f73cb84aac894443628231b8e
Angel Gomez <a22angelgp@iessanclemente.net>
2022-11-09 10:31:01 +0100

    clase cosas

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -1,34 +1,21 @@
-import javax.sound.midi.MidiChannel;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Synthesizer;
-public class JavaTests {
-    static class Sonido{
-        public Sonido(int instrumento){
-            try {
-                Synthesizer synth = MidiSystem.getSynthesizer();
-                synth.open();
-                MidiChannel miCanal = synth.getChannels()[0];
-                int volume = 80; // entre 0 y 127
-                int duration = 200; // en milisegundos
-                miCanal.programChange(instrumento);
-                int[] notas={60,62,64,65,67,69,71,72};
-                for(int nota: notas){
-                    miCanal.noteOn( nota, volume );
-                    Thread.sleep(duration);
-                    miCanal.noteOff(nota);
-                }
-                Thread.sleep( 500 );
-                synth.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-    public static class Unidad2{
-        public static void main( String[] args ) {
-            new Sonido(39);
-        }
-    }
+import java.util.Scanner;
 
+public class JavaTests {
+    public static void main(String[] args) {
+        Scanner uwu = new Scanner(System.in);
+        String[] owo = uwu.nextLine().split(" ");
+        String comidadiaria=owo[0];
+        String animales=owo[1];
+        String kilosdiarios=owo[2];
+        double uwu1=Double.parseDouble(comidadiaria);
+        double uwu2=Double.parseDouble(animales);
+        double uwu3=Double.parseDouble(kilosdiarios);
+        double necesaria=uwu2*uwu3;
+        double pito=uwu1-necesaria;
+        if (uwu1<necesaria){
+            System.out.println("Ración: "+necesaria/uwu1+" Kg");
+        }else System.out.println("Excedente: "+pito);
+
+    }
 
 }
