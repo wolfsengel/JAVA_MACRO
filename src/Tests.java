206e5127008da2cb3abc97cd118fd36997afff1c
Angel Gomez <a22angelgp@iessanclemente.net>
2022-11-15 14:29:41 +0100

    clase cosas

--- src/Tests.java
+++ src/Tests.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
 public class Tests {
+        public static void main(String[] args) {
+            int ubuntu;
+            do {
+                Scanner uwu = new Scanner(System.in);
+                ubuntu = uwu.nextInt();
+                Scanner lozano = new Scanner(System.in);
+                int loci;
+                int sum = 0;
+                for (int i = 0; i < ubuntu; i++) {
+                    loci = lozano.nextInt();
+                    sum = loci + sum;
+                }
+                if (ubuntu != 0) {
+                    System.out.println(sum);
+                }
+
+            } while (ubuntu != 0);
+
+        }
 }
