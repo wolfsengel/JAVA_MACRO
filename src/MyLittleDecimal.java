b8befaa137e5fa75a7c453d8b1d35ef1aa882b42
angel <angel.gomez.prol@udc.es>
2022-11-17 18:22:19 +0100

    midecimal collons

--- src/MyLittleDecimal.java
+++ src/MyLittleDecimal.java
@@ -1,3 +1,7 @@
+import java.util.Objects;
+
+import static java.lang.Math.abs;
+
 public class MyLittleDecimal {
     int noomeroo;
     MyLittleDecimal(String numuru){
@@ -8,12 +12,60 @@ public class MyLittleDecimal {
     int getValor(){
         return this.noomeroo;
     }
+    public String deINTaFdouble(int noomeroo){
+        if (noomeroo==0){
+            return "0,00";
+        }
+        String num=noomeroo+" ";
+        String[] ary= num.split("");
+        if (Objects.equals(ary[0], "-")){
+            num=num.replaceAll("-","");
+            ary= num.split("");
+        }
+        if (ary.length==2){
+            if (noomeroo<0){
+                noomeroo=abs(noomeroo);
+                String num2="00"+noomeroo+"0";
+                String[] ary2= num2.split("");
+                ary2[ary2.length-1]=ary2[ary2.length-2];
+                ary2[ary2.length-2]=ary2[ary2.length-3];
+                ary2[ary2.length-3]=",";
+                return "-"+String.join("", ary2);
+            }
+            String num2="00"+noomeroo+"0";
+            String[] ary2= num2.split("");
+            ary2[ary2.length-1]=ary2[ary2.length-2];
+            ary2[ary2.length-2]=ary2[ary2.length-3];
+            ary2[ary2.length-3]=",";
+            return String.join("", ary2);
+        }
+        if (ary.length==3){
+            if (noomeroo<0){
+                noomeroo=abs(noomeroo);
+                String num2="0"+noomeroo+"0";
+                String[] ary2= num2.split("");
+                ary2[ary2.length-1]=ary2[ary2.length-2];
+                ary2[ary2.length-2]=ary2[ary2.length-3];
+                ary2[ary2.length-3]=",";
+                return "-"+String.join("", ary2);
+            }
+            String num2="0"+noomeroo+"0";
+            String[] ary2= num2.split("");
+            ary2[ary2.length-1]=ary2[ary2.length-2];
+            ary2[ary2.length-2]=ary2[ary2.length-3];
+            ary2[ary2.length-3]=",";
+            return String.join("", ary2);
+        }
+        ary[ary.length-1]=ary[ary.length-2];
+        ary[ary.length-2]=ary[ary.length-3];
+        ary[ary.length-3]=",";
+        if (noomeroo<0){
+            return "-"+String.join("", ary);
+        }else return String.join("", ary);
+    }
     @Override
     public String toString() {
-            double owo = noomeroo;
-            double resultado= owo/100;
-            String result = String.format("%.2f", resultado);
-            return result.replaceAll("\\.",",");
+        return deINTaFdouble(this.noomeroo);
     }
     MyLittleDecimal sumar(MyLittleDecimal cum){
         double noomeroo1 = this.noomeroo;
@@ -24,7 +76,9 @@ public class MyLittleDecimal {
     MyLittleDecimal restar(MyLittleDecimal cum){
         double noomeroo1 = this.noomeroo;
         double noomeroo2 = cum.noomeroo;
-        return new MyLittleDecimal(String.valueOf((noomeroo1-noomeroo2)/100));
+        if (this.noomeroo==cum.noomeroo){
+            return new MyLittleDecimal("000");
+        } else return new MyLittleDecimal(String.valueOf((noomeroo1-noomeroo2)/100));
     }
     MyLittleDecimal multiplicar(MyLittleDecimal cum){
         double noomeroo1 = this.noomeroo;
@@ -38,16 +92,9 @@ public class MyLittleDecimal {
     }
 
     public static void main(String[] args) {
-        MyLittleDecimal owo=new MyLittleDecimal("210.11");
-        System.out.println(owo);
-        MyLittleDecimal pollo=owo.sumar(owo);
-        System.out.println(pollo);
-        pollo=owo.restar(owo);
-        System.out.println(pollo);
-        pollo=owo.multiplicar(owo);
-        System.out.println(pollo);
-        MyLittleDecimal uwu=new MyLittleDecimal("30.45");
-        int penenene=uwu.getValor();
-        System.out.println(penenene);
+        MyLittleDecimal mld1 = new MyLittleDecimal("100,00");
+        MyLittleDecimal mld2 = new MyLittleDecimal("300,00");
+        MyLittleDecimal mld3=mld1.dividir(mld2);
+        System.out.println(mld3);
     }
 }
---
+++ src/Tests2.java
@@ -0,0 +1,6 @@
+public class Tests2 {
+    public static void main(String[] args) {
+        String[] owo = "1000".split("");
+
+    }
+}
