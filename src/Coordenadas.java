118dd4153c5a287911837111537daa7740aceb2e
angel <angel.gomez.prol@udc.es>
2022-11-22 18:43:26 +0100

    midecimal collons

---
+++ src/Coordenadas.java
@@ -0,0 +1,19 @@
+public class Coordenadas {
+      int x;
+      int y;
+      double dividir(){
+              double pene=this.x;
+              double laura=this.y;
+              return pene/laura;
+      }
+      Coordenadas(){
+              this.x=0;
+              this.y=0;
+      }
+        public static void main(String[] args) {
+              Coordenadas punto= new Coordenadas();
+              punto.x=10;
+              punto.y=8;
+                System.out.print(punto.dividir());
+        }
+}
