dde9150f42c41182b8668c594fd96fb8aad536a3
angel <angel.gomez.prol@udc.es>
2022-11-27 16:35:36 +0100

    rematamos ultimas cosas

--- src/tareaproclase/todio.java
+++ src/tareaproclase/todio.java
@@ -4,49 +4,49 @@ import java.util.Scanner;
 
 public class todio {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-        int t=0,e=0,o=0,d=0,i=0;
-        Scanner uwu= new Scanner(System.in);
-        String[] bloque=uwu.nextLine().split("");
-        if (Objects.equals(bloque[0], "4")){
+        int numCasos = Integer.parseInt(s.nextLine());
+        int count = 0;
+        int contadorBusqueda;
+        if (numCasos == 4) {
             System.out.println("SI");
-            return;
         }
-        //--------------------------------------------------------------------------------
-        for (int j=0;j< bloque.length;j++) {
-            if (Objects.equals(bloque[j], "t")){
-                t+=1;
-                for (int g=j;g< bloque.length;g++) {
-                    if (Objects.equals(bloque[g], "e")){
-                        e+=1;
-                        for (int q=g;q< bloque.length;q++) {
-                            if (Objects.equals(bloque[q], "o")){
-                                o+=1;
-                                for (int w=q;w< bloque.length;w++) {
-                                    if (Objects.equals(bloque[w], "d")){
-                                        d+=1;
-                                        for (int m=w;m< bloque.length;m++) {
-                                            if (Objects.equals(bloque[m], "t")){
-                                                o+=1;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+        if (numCasos == 0) {
+        } else {
+
+            do {
+
+                String texto = s.nextLine();
+                String buscar = s.nextLine();
+
+                texto = texto.replaceAll(" ", "");
+                texto = texto.toLowerCase();
+
+                buscar = buscar.replaceAll(" ", "");
+                buscar = buscar.toLowerCase();
+                int indexUltimoCaracter = 0;
+                contadorBusqueda = 0;
+                for (int i = 0; i < buscar.length(); i++) {
+
+                    for (int j = indexUltimoCaracter; j < texto.length(); j++) {
+                        if (buscar.charAt(i) == texto.charAt(j)) {
+                            indexUltimoCaracter = j + 1; //
+                            contadorBusqueda++;
+                            break;
                         }
                     }
                 }
-            }
+
+                count++;
+                if (contadorBusqueda == buscar.length()) {
+                    System.out.println("SI");
+                } else {
+                    System.out.println("NO");
+                }
+            } while (count != numCasos);
+
         }
-        for (int j=0;j< bloque.length;j++) {
-            if (!Objects.equals(bloque[j], "i")){
-                bloque[j]="";
-            }
-        }
-        //--------------------------------------------------------------------------------
-        for (String s : bloque){
-            System.out.print(s);
-        }
+
     }
 }
