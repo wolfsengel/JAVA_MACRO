91909439448639eddb50950797460f02b9181167
angel <angel.gomez.prol@udc.es>
2022-12-04 00:32:37 +0100

    RISK PASOS

--- src/tareaproclase/Risk.java
+++ src/tareaproclase/Risk.java
@@ -2,12 +2,11 @@ package tareaproclase;
 
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Risk {
-    public static int[] sort(int[] list){
+    public static void sort(int[] list){
         for (int i = 0; i < list.length; i++) {
             for (int j = i + 1; j < list.length; j++) {
                 int temp;
@@ -18,42 +17,78 @@ public class Risk {
                 }
             }
         }
-        return list;
     }
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int[] lin1=new int[5];
-        for(int i=0;i<5;i++){
-            lin1[i]=sc.nextInt();
-        }
-        int td=lin1[0];
-        int ta=lin1[1];
-        int dd=lin1[2];
-        int da=lin1[3];
-        int no=lin1[4];
-        int[] defence = new int[td];
-        int[] attack = new int[ta];
-        int[] diceD = new int[dd];
-        int[] diceA = new int[da];
-        int[] waves = new int[no];
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
 
-        for (int i = 0;i<td;i++){
-            defence[i]=sc.nextInt();
-        }
-        sort(defence);
-        for (int i = 0;i<ta;i++){
-            attack[i]=sc.nextInt();
-        }
-        sort(attack);
-        int ata = Math.min(attack.length, defence.length);
-        for (int i=0;i<ata;i++){
-            if (attack[i]<defence[i]){
-
-            } else if (defence[i]<attack[i]) {
-                
+            int[] lin1 = new int[5];
+            for (int i = 0; i < 5; i++) {
+                lin1[i] = sc.nextInt();
             }
+            int td = lin1[0];
+            int ta = lin1[1];
+            int dd = lin1[2];
+            int da = lin1[3];
+            int no = lin1[4];
+            int[] defence = new int[td];
+            int[] attack = new int[ta];
+
+            for (int i = 0; i < td; i++) {
+                defence[i] = sc.nextInt();
+            }
+            sort(defence);
+            for (int i = 0; i < ta; i++) {
+                attack[i] = sc.nextInt();
+            }
+            sort(attack);
+            int ata;
+            int k = 0;
+            ata = Math.min(attack.length, defence.length);
+            while (k < no){
+                for (int i = 0; i < ata; i++) {
+                    if (attack[i] < defence[i]) {
+                        attack[i] = 0;
+                    } else if (defence[i] < attack[i]) {
+                        defence[i] = 0;
+                    } else if (defence[i] == attack[i]) {
+                        attack[i] = 0;
+                    }
+                }
+                System.out.println(Arrays.toString(defence));
+                System.out.println(Arrays.toString(attack));
+                for (int j = 0; j < attack.length; j++) {
+                    if (attack[j] == 0 && da>0) {
+                        attack[j] = sc.nextInt();
+                        da--;
+                    }
+                }
+                for (int j = 0; j < defence.length; j++) {
+                    if (attack[j] == 0 && dd>0) {
+                        attack[j] = sc.nextInt();
+                        dd--;
+                    }
+                }
+                sort(defence);
+                sort(attack);
+                k++;
+            }
+            System.out.println(Arrays.toString(defence));
+            System.out.println(Arrays.toString(attack));
+
+            int defc=0,attc=0;
+            for (int i : attack) {
+                if (i != 0) {
+                    attc++;
+                }
+            }
+            for (int i : defence) {
+                if (i != 0) {
+                    defc++;
+                }
+            }
+            System.out.println(defc+" "+attc);
         }
-        System.out.println(Arrays.toString(defence));
-        System.out.println(Arrays.toString(attack));
+
     }
 }
