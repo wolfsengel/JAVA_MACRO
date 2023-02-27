8f426ac53d9010610da7d541be15a247f1ffe0f2
Angel Gomez <a22angelgp@iessanclemente.net>
2023-02-27 09:37:21 +0100

    STRATEGY PATRON

--- src/Strategy/CreditCardPayment.java
+++ src/Strategy/CreditCardPayment.java
@@ -1,5 +1,39 @@
 package Strategy;
 
+import java.util.Scanner;
+
 public class CreditCardPayment implements PaymentStrategy{
+    int numeroT;
+    int codeT;
+    String fechaCT;
+
+    public CreditCardPayment(int numeroT, int codeT, String fechaCT) {
+        this.numeroT = numeroT;
+        this.codeT = codeT;
+        this.fechaCT = fechaCT;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCardPayment" +
+                "numeroT=" + numeroT +
+                ", codeT=" + codeT +
+                ", fechaCT='" + fechaCT + '\'';
+    }
+
+    @Override
+    public boolean pay() {
+        Scanner sc = new Scanner(System.in);
+        try{
+            int num= sc.nextInt();
+            int code= sc.nextInt();
+            String fech =sc.nextLine();
+            CreditCardPayment credit = new CreditCardPayment(num, code, fech);
+            System.out.println(credit);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
 
---
+++ src/Strategy/PaymentProcessor.java
@@ -0,0 +1,8 @@
+package Strategy;
+
+public class PaymentProcessor {
+
+    public void processpayment(){
+
+    }
+}
